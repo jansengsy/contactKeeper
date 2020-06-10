d77089fbe44ff39579694b529dc57240a6125c68
kees <keessqq@git.training>
2020-06-10 17:03:28 +0100

    Added contact context/reducer/state, and contacts/contact item components

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import { Home } from './components/pages/Home';
 import { About } from './components/pages/About';
+
+import ContactState from './Context/Contact/ContactState';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment className='App'>
-        <NavBar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment className='App'>
+          <NavBar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
