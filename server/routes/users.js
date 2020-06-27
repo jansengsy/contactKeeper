b180659ea8387776717e139e81c6165f2c4ff53b
kees <keessqq@git.training>
2020-06-27 16:13:28 +0100

    Attempting to use docker compose

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('../../config/default.json');
+const config = require('../config/default.json');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
